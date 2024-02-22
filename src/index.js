2f02267ee858545eb65f87893f3f594a5b6026a1
Ranjeetarswa <150023714+Ranjeetarswa@users.noreply.github.com>
2024-02-22 14:17:52 +0530

    utlit add

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import { addListener } from "nodemon"
 
 dotenv.config({
     path: './env'
@@ -9,6 +10,15 @@ dotenv.config({
 
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️  Server is runing at port : ${process.env.PORT}`);
+    })
+})
+
+.catch(() => {
+    console.log("MONGO db connection failed !!!", err);
+})
 
 
 /*
