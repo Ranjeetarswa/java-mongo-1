c7beb859fa0c7329348f1a5fc13a236658d91acd
Ranjeetarswa <150023714+Ranjeetarswa@users.noreply.github.com>
2024-02-22 10:38:29 +0530

    connect to mongo atles

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const connectDB = async () => {
+
+    try{
+      const connactionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+      console.log(`/n MongoDB connected !! DB HOST: ${connactionInstance.connection.host}`);
+    }catch(error){
+         console.log("MONGODB connaction FAILED", error);
+         process.exit(1)
+    }
+}
+
+export default connectDB
+
+
